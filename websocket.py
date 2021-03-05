69dfd470be6d3ea62fca2d432b22a5337cc2ea7b
lordslair <5469390+lordslair@users.noreply.github.com>
2021-03-05 14:12:42 +0100

    Added logging and comments

--- websocket.py
+++ websocket.py
@@ -8,7 +8,11 @@ import time
 import websockets
 import yarqueue
 
-from redis import Redis
+from redis              import Redis
+from datetime           import datetime
+
+# Shorted definition for actual now() with proper format
+def mynow(): return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 # Redis variables
 REDIS_HOST    = os.environ['SEP_BACKEND_REDIS_SVC_SERVICE_HOST']
@@ -20,12 +24,25 @@ REDIS_SLEEP   = int(os.environ['SEP_REDIS_SLEEP']) # We receive k8s env as strin
 WSS_HOST      = os.environ['SEP_WSS_HOST']
 WSS_PORT      = os.environ['SEP_WSS_PORT']
 
-r = Redis(host     = REDIS_HOST,
-          port     = REDIS_PORT,
-          db       = REDIS_DB_NAME,
-          encoding = 'utf-8')
+# Opening Redis connection
+try:
+    r = Redis(host     = REDIS_HOST,
+              port     = REDIS_PORT,
+              db       = REDIS_DB_NAME,
+              encoding = 'utf-8')
+except:
+    print(f'{mynow()} [core] Connection to redis [✗]')
+else:
+    print(f'{mynow()} [core] Connection to redis [✓]')
 
-yqueue = yarqueue.Queue(name="broadcast", redis=r)
+# Opening Queue
+try:
+    yqueue_name = 'broadcast'
+    yqueue      = yarqueue.Queue(name=yqueue_name, redis=r)
+except:
+    print(f'{mynow()} [core] Connection to yarqueue:{yqueue_name} [✗]')
+else:
+    print(f'{mynow()} [core] Connection to yarqueue:{yqueue_name} [✓]')
 
 CLIENTS = set()
 
@@ -33,7 +50,7 @@ async def broadcast():
     while True:
         if not yqueue.empty():
             data = yqueue.get()
-            print (f'consumer processing: <{data}>')
+            print(f'{mynow()} [q:broadcast] Consumer got from redis:<{data}>')
             await asyncio.gather(
             *[ws.send(data) for ws in CLIENTS],
             return_exceptions=False,)
@@ -42,23 +59,31 @@ async def broadcast():
 
 async def handler(websocket, path):
     CLIENTS.add(websocket)
-    print("add client")
+    print(f'{mynow()} [core] Client connected')
+    print(f'{websocket.remote_address}')
     try:
         async for msg in websocket:
             yqueue.put(msg)
     except websockets.ConnectionClosedError:
-        print("Connection closed")
+        print(f'{mynow()} [core] Connection closed')
     finally:
         CLIENTS.remove(websocket)
-        print("remove client")
+        print(f'{mynow()} [core] Client removed')
 
 loop = asyncio.get_event_loop()
 loop.create_task(broadcast())
 
-start_server = websockets.serve(handler, WSS_HOST, WSS_PORT)
+# Opening Queue
+try:
+    start_server = websockets.serve(handler, WSS_HOST, WSS_PORT)
+except:
+    print(f'{mynow()} [core] Starting websocket [✗]')
+else:
+    print(f'{mynow()} [core] Starting websocket [✓]')
 
+# Looping to daemonize the Queue
 try:
     loop.run_until_complete(start_server)
     loop.run_forever()
 except KeyboardInterrupt:
-    print("Exiting")
+    print(f'{mynow()} [core] Exiting')
